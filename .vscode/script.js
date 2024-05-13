740d8dca36677d74b55bafe170ccaf1080a5248d
WCMHunter <willcrz@gamil.com>
2024-05-13 20:49:59 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // if (html.classList.contains('light')) {
+  // html.classList.remove('light')
+  // } else {
+  //  html.classList.add('light')
+  // }
+
+  //pegar a tag img 
+  const img = document.querySelector("#profile img")
+  //substituir a img
+  if (html.classList.contains('light')) {
+    //se tiver light mode, adicionar a img light
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'Foto do Mayke Brito sorrindo usando óculos escuros e jaqueta preta, sem barba e fundo colorido');
+  } else {
+    //se tiver sem light mode, adicionar a img normal
+    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('alt', 'Foto do Mayke Brito sorrindo usando óculos e camisa preta, barba e fundo amarelo');
+   }
+}
